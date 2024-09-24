64c48ce9105c681f3bef509c888d82d7a95bb12a
seongjun-jin <wlstjdwns2108@naver.com>
2024-09-24 12:12:14 +0900

    원 운동 구현 완료

--- character_move.py
+++ character_move.py
@@ -1,4 +1,5 @@
 from pico2d import *
+import math
 
 open_canvas()
 
@@ -6,10 +7,17 @@ boy = load_image('character.png')
 grass = load_image('grass.png')
 
 def run_circle():
-    clear_canvas_now()
-    boy.draw_now(400,300)
-    delay(5)
-
+    r, cx, cy = 300, 800//2, 600//2
+    
+    for degree in range(0, 360, 5):
+        theta = math.radians(degree)
+        x = r * math.cos(theta) + cx
+        y = r * math.sin(theta) + cy
+        
+        clear_canvas_now()
+        boy.draw_now(x,y)
+        delay(0.1)
+    
 def run_Rectangle():
     
     pass
