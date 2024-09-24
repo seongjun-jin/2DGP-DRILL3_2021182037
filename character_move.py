56ca7db0eeb1a50121cf19e5d3c898aae0733b99
seongjun-jin <wlstjdwns2108@naver.com>
2024-09-24 12:39:11 +0900

    사각운동 완료 삼각형 시작

--- character_move.py
+++ character_move.py
@@ -9,7 +9,7 @@ grass = load_image('grass.png')
 def draw_boy(x,y):
     clear_canvas_now()
     boy.draw_now(x,y)
-    delay(0.05)
+    delay(0.01)
 
 def run_circle():
     r, cx, cy = 300, 800//2, 600//2
@@ -25,16 +25,16 @@ def run_top():
         draw_boy(x,550)
     
 def run_right():
-    print("RIGHT")
-    pass
-
-def run_left():
-    print("LEFT")
-    pass
+    for y in range(550, 0, -10):
+        draw_boy(790, y)
 
 def run_bottom():
-    print("BOTTOM")
-    pass
+    for x in range(800, 0, -10):
+        draw_boy(x,10)
+
+def run_left():
+    for y in range(0, 550, 10):
+        draw_boy(10, y)
 
 def run_Rectangle():
     run_top()
@@ -44,7 +44,7 @@ def run_Rectangle():
     
 
 while True:
-    run_circle()
+    #run_circle()
     run_Rectangle()
     break
     
