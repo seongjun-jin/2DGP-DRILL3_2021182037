94b6e185d9d979f73d43b270fae8aa828c64a6a7
seongjun-jin <wlstjdwns2108@naver.com>
2024-09-24 12:33:52 +0900

    사각형 윗줄이동 완성

--- character_move.py
+++ character_move.py
@@ -6,6 +6,11 @@ open_canvas()
 boy = load_image('character.png')
 grass = load_image('grass.png')
 
+def draw_boy(x,y):
+    clear_canvas_now()
+    boy.draw_now(x,y)
+    delay(0.05)
+
 def run_circle():
     r, cx, cy = 300, 800//2, 600//2
     
@@ -13,14 +18,12 @@ def run_circle():
         theta = math.radians(degree)
         x = r * math.cos(theta) + cx
         y = r * math.sin(theta) + cy
+        draw_boy(x,y)
         
-        clear_canvas_now()
-        boy.draw_now(x,y)
-        delay(0.01)
 def run_top():
-    print("TOP")
-    pass
-
+    for x in range(0, 800, 10):
+        draw_boy(x,550)
+    
 def run_right():
     print("RIGHT")
     pass
@@ -41,7 +44,7 @@ def run_Rectangle():
     
 
 while True:
-    #run_circle()
+    run_circle()
     run_Rectangle()
     break
     
